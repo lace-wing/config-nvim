7678522c4015107e5afa8e0c8f9ff90ae7ed2949
lace-wing <lycrlsu01@gmail.com>
2025-01-21 16:23:30 -0800

    update

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -1,12 +1,12 @@
 vim.opt_local.spell = true
 vim.opt_local.spelllang = "en,cjk"
 
-local git_root_cmd = "git rev-parse --show-toplevel"
-local root = vim.fn.expand("%:p:h")
-if os.execute(git_root_cmd) == 0 then
-  root = vim.fn.system(git_root_cmd)
-end
-
+-- local git_root_cmd = "git rev-parse --show-toplevel"
+-- local root = vim.fn.expand("%:p:h")
+-- if os.execute(git_root_cmd) == 0 then
+--   root = vim.fn.system(git_root_cmd)
+-- end
+--
 -- vim.api.nvim_create_autocmd(
 --   "BufWritePost", {
 --     pattern = "main.typ",
